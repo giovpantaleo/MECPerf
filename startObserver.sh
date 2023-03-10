000fca45d524859b8ca7b7d7e4ffd1ef9142b307
Giovanni Pantaleo <127411857+giovpantaleo@users.noreply.github.com>
2023-03-10 11:43:20 +0100

    master

--- startObserver.sh
+++ startObserver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-java -jar Observer.jar --aggregator-ip "192.168.122.3" --aggregator-port 6766 --remote-ip  "192.168.122.4"  --remote-cmd-port 6789 --remote-tcp-port 6788 --remote-udp-port 6787 --observer-cmd-port 6792 --observer-tcp-port 6791 --observer-udp-port 6790
+#java -jar Observer.jar --aggregator-ip "192.168.122.3" --aggregator-port 6766 --remote-ip  "192.168.122.4"  --remote-cmd-port 6789 --remote-tcp-port 6788 --remote-udp-port 6787 --observer-cmd-port 6792 --observer-tcp-port 6791 --observer-udp-port 6790
 
 #R2LAB 
 # Aggregator node 11
