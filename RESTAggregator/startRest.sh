7adb6bea3d16daa9f5ecd4e1c3d7b2cb54ced7e0
Giovanni Pantaleo <127411857+giovpantaleo@users.noreply.github.com>
2023-03-10 11:29:37 +0100

    master

--- RESTAggregator/startRest.sh
+++ RESTAggregator/startRest.sh
@@ -3,7 +3,7 @@
 ##nohup waitress-serve --host="" --port=5001 --call "RESTAggregator:create_app" &
 
 # R2LAB
-pip install /home/MECPerf/MECPerf-master/RESTAggregator-1.0.0-py2-none-any.whl 
+pip install /home/MECPerf/MECPerf-master/RESTAggregator/dist/RESTAggregator-1.0.0-py2-none-any.whl 
 nohup waitress-serve --host="192.168.3.11" --port=5001 --call "RESTAggregator:create_app" &
 ##nohup waitress-serve --host="127.0.0.1" --port=5001 --call "RESTAggregator:create_app" &
 
