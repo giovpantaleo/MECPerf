88ae6e5c526600663c0a6b8cbaa973fab6c5ece9
Giovanni Pantaleo <127411857+giovpantaleo@users.noreply.github.com>
2023-03-21 16:42:29 +0100

    master

--- aggregator/src/main/java/it/unipi/dii/aggregator/Aggregator.java
+++ aggregator/src/main/java/it/unipi/dii/aggregator/Aggregator.java
@@ -131,7 +131,7 @@ public class Aggregator {
                         System.out.println(arrayJson);
                         System.out.println(temp.keySet());
 
-                    }catch{
+                    }catch(Exception e){
                         System.out.println("Allert");
                     }
                 }
