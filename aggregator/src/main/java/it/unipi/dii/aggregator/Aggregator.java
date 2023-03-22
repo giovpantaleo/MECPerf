dce129915d69c2b051c63240dbed10e4694168da
Giovanni Pantaleo <127411857+giovpantaleo@users.noreply.github.com>
2023-03-22 10:17:37 +0100

    master

--- aggregator/src/main/java/it/unipi/dii/aggregator/Aggregator.java
+++ aggregator/src/main/java/it/unipi/dii/aggregator/Aggregator.java
@@ -139,8 +139,8 @@ public class Aggregator {
                 Set<String> keysFirstLevel = new HashSet<String>(objJs.keySet());
                 for (int i = 0; i<keysFirstLevel.size(); i++){
                     try{
-                        System.out.println(objJs.getJSONObject(i));
-                        System.out.println(objJs.getJSONObject(i).getClass());
+                        System.out.println(objJs.get(keysFirstLevel(i));
+                        System.out.println(objJs.get(keysFirstLevel(i)).getClass());
 
                     }catch(Exception e){
                         System.out.println("Allert");
