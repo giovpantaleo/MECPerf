d1c140fdfe57b50f352cab6053d21eb66150e332
Giovanni Pantaleo <127411857+giovpantaleo@users.noreply.github.com>
2023-03-24 10:57:47 +0100

    master

--- startLocal.sh
+++ startLocal.sh
@@ -9,16 +9,18 @@ java -cp commons-cli-1.4.jar:CommandLineApp.jar it.unipi.dii.mecperfng.commandli
 #without interface
 #java -cp commons-cli-1.4.jar:CommandLineApp.jar it.unipi.dii.mecperfng.commandlineapp.CommandLineApp --observer-address 192.168.3.1 --measure-type all --direction all --keyword "prova insermento REST" --command-port 6792 --tcp-port 6791 --udp-port 6790  --packet-size-tcp-bandwidth 1420 --num-packet-tcp-bandwidth 30  --num-tests-udp-capacity 3 --packet-size-udp-capacity 1420 --packet-size-tcp-latency 1 --num-packet-tcp-latency 4 --packet-size-udp-latency 1 --num-packet-udp-latency 5 --timer-interval 100 --node-id "TESTNODE" --crosstraffic "OMbps" --observer-position "edge" --access-technology "wifi"  --number-of-clients 1 
 
-
+echo "i am here"
 
 SELECT Timestamp AS "time",        (1.0 *(SUM(kBytes) * 1024 *1024 )/(1.0 * SUM(nanoTimes) / 1000000000)) AS "MEC -> Cloud - MBps" FROM Test INNER JOIN BandwidthMeasure ON Test.ID = BandwidthMeasure.id WHERE (SenderIdentity = 'Observer' AND ReceiverIdentity = 'Server' AND Command = 'TCPBandwidth') GROUP BY Test.ID ORDER BY time desc limit 10;
 
 
 for j in 0 1 2 3 4 5 6 7 8 9 
+    echo "i am here"
     do
 #java -cp commons-cli-1.4.jar:CommandLineApp.jar it.unipi.dii.mecperfng.commandlineapp.CommandLineApp --observer-address 131.114.73.2 --measure-type bandwidthTCP --direction sender --keyword "Validazione REST" --command-port 6792 --tcp-port 6791 --udp-port 6790  --packet-size-tcp-bandwidth 1420 --num-packet-tcp-bandwidth 1024  --num-tests-udp-capacity 1000 --packet-size-udp-capacity 1420 --packet-size-tcp-latency 1 --num-packet-tcp-latency 100 --packet-size-udp-latency 1 --num-packet-udp-latency 100 --node-id "TESTNODE" --interface-name "eth0" --crosstraffic "OMbps" --observer-position "edge" --access-technology "wifi"  --number-of-clients 1 
 java -cp commons-cli-1.4.jar:CommandLineApp.jar it.unipi.dii.mecperfng.commandlineapp.CommandLineApp --observer-address 192.168.3.1 --measure-type bandwidthTCP --direction sender --keyword "Validazione REST" --command-port 6792 --tcp-port 6791 --udp-port 6790  --packet-size-tcp-bandwidth 1420 --num-packet-tcp-bandwidth 1024  --num-tests-udp-capacity 1000 --packet-size-udp-capacity 1420 --packet-size-tcp-latency 1 --num-packet-tcp-latency 100 --packet-size-udp-latency 1 --num-packet-udp-latency 100 --node-id "TESTNODE" --interface-name "control" --crosstraffic "OMbps" --observer-position "edge" --access-technology "wifi"  --number-of-clients 1 
 done; for j in 0 1 2 3 4 5 6 7 8 9 
+    echo "i am here"
     do
 #java -cp commons-cli-1.4.jar:CommandLineApp.jar it.unipi.dii.mecperfng.commandlineapp.CommandLineApp --observer-address 131.114.73.2 --measure-type bandwidthUDP --direction sender --keyword "Validazione REST" --command-port 6792 --tcp-port 6791 --udp-port 6790  --packet-size-tcp-bandwidth 1420 --num-packet-tcp-bandwidth 1024  --num-tests-udp-capacity 1000 --packet-size-udp-capacity 1420 --packet-size-tcp-latency 1 --num-packet-tcp-latency 100 --packet-size-udp-latency 1 --num-packet-udp-latency 100 --node-id "TESTNODE" --interface-name "eth0" --crosstraffic "OMbps" --observer-position "edge" --access-technology "wifi"  --number-of-clients 1 
 java -cp commons-cli-1.4.jar:CommandLineApp.jar it.unipi.dii.mecperfng.commandlineapp.CommandLineApp --observer-address 192.168.3.1 --measure-type bandwidthUDP --direction sender --keyword "Validazione REST" --command-port 6792 --tcp-port 6791 --udp-port 6790  --packet-size-tcp-bandwidth 1420 --num-packet-tcp-bandwidth 1024  --num-tests-udp-capacity 1000 --packet-size-udp-capacity 1420 --packet-size-tcp-latency 1 --num-packet-tcp-latency 100 --packet-size-udp-latency 1 --num-packet-udp-latency 100 --node-id "TESTNODE" --interface-name "control" --crosstraffic "OMbps" --observer-position "edge" --access-technology "wifi"  --number-of-clients 1 
