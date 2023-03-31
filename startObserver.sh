5431956093d9f811d1acca92c62418ebf1f84f89
Giovanni Pantaleo <127411857+giovpantaleo@users.noreply.github.com>
2023-03-31 11:05:30 +0200

    master

--- startObserver.sh
+++ startObserver.sh
@@ -3,10 +3,10 @@
 
 #R2LAB correct
 # Aggregator node 11
-#java -jar Observer.jar --aggregator-ip "192.168.3.11" --aggregator-port 6766 --remote-ip  "192.168.3.11"  --remote-cmd-port 6789 --remote-tcp-port 6788 --remote-udp-port 6787 --observer-cmd-port 6792 --observer-tcp-port 6791 --observer-udp-port 6790
+java -jar Observer.jar --aggregator-ip "192.168.3.11" --aggregator-port 6766 --remote-ip  "192.168.3.11"  --remote-cmd-port 6789 --remote-tcp-port 6788 --remote-udp-port 6787 --observer-cmd-port 6792 --observer-tcp-port 6791 --observer-udp-port 6790
 
 # Aggregator node 8
 ##java -jar Observer.jar --aggregator-ip "192.168.3.8" --aggregator-port 6766 --remote-ip  "192.168.3.11"  --remote-cmd-port 6789 --remote-tcp-port 6788 --remote-udp-port 6787 --observer-cmd-port 6792 --observer-tcp-port 6791 --observer-udp-port 6790
 
 # Aggregator node 17, rms 26
-java -jar Observer.jar --aggregator-ip "192.168.3.17" --aggregator-port 6766 --remote-ip  "192.168.3.26"  --remote-cmd-port 6789 --remote-tcp-port 6788 --remote-udp-port 6787 --observer-cmd-port 6792 --observer-tcp-port 6791 --observer-udp-port 6790
+#java -jar Observer.jar --aggregator-ip "192.168.3.17" --aggregator-port 6766 --remote-ip  "192.168.3.26"  --remote-cmd-port 6789 --remote-tcp-port 6788 --remote-udp-port 6787 --observer-cmd-port 6792 --observer-tcp-port 6791 --observer-udp-port 6790
