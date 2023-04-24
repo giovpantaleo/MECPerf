837a41bc6610dfb32546b3be673145c72a4793de
giovanni pantaleo <giovannipantaleo00@gmail.com>
2023-04-24 10:37:38 +0200

    obs test

--- observer/src/main/java/it/unipi/dii/observer/Observer.java
+++ observer/src/main/java/it/unipi/dii/observer/Observer.java
@@ -1350,7 +1350,7 @@ public class Observer {
                                       
         //csv DEB   id | sub_id | nanoTimes | kBytes
         try (FileWriter writer = new FileWriter("measure_bw_obs_side.csv", true)) {
-            String toWrite = "first segment \n";
+            String toWrite = "first segment sender: " + metadataFirstSegment.get("Sender-identity") +", receiver: "+  metadataFirstSegment.get("Receiver-identity")+"\n";
             writer.write(toWrite);
             for (Map.Entry<Integer, Long[]> entry : map1.entrySet()) {
                 String ToWrite = entry.getKey().toString()+","; //id,sub_id,
@@ -1360,7 +1360,8 @@ public class Observer {
                 }//deb
                 writer.write(ToWrite+"\n");
             }
-            String toWrite2 = "second segment \n";
+            String toWrite2 = "second segment sender: " + metadataSecondSegment.get("Sender-identity") +", receiver: "+  metadataSecondSegment.get("Receiver-identity")+"\n";
+
             writer.write(toWrite2);
             for (Map.Entry<Integer, Long[]> entry : map2.entrySet()) {
                 String ToWrite2 = entry.getKey().toString()+","; //id,sub_id,
